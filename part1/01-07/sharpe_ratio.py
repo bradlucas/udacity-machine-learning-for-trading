b8119bd0fd828b9e9efa34828a653dd3b99c88bb
Brad Lucas <brad@beaconhill.com>
2017-03-29 22:42:23 -0400

    part1/01-07

---
+++ part1/01-07/sharpe_ratio.py
@@ -0,0 +1,11 @@
+import math
+import numpy as np
+
+avg_daily_rets=0.001
+daily_rf=0.0002
+std_daily_rets=0.001
+
+
+sharpe = math.sqrt(252) * np.mean(avg_daily_rets - daily_rf) / std_daily_rets
+
+print sharpe
